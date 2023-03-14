15f201cae0d6c1f7d471926099e521f878c0d31b
totu112000 <96477917+totu112000@users.noreply.github.com>
2023-03-14 16:34:56 +0700

    Update Bai8.java

--- BaiThucHanhLab2/src/Bai8.java
+++ BaiThucHanhLab2/src/Bai8.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 
 public class Bai8 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhap so nguyen n ");
-        int n = scanner.nextInt();
-        while (n > 0) {
-            int tong += a % 10;
-            n /= 10;
-        int tbc =tong/2
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap vao so phan tu cua day: ");
+        int n = sc.nextInt();
+        if (n <= 0) {
+            System.out.println("so khong hop le");
+            return;
         }
-        System.out.println("trung binh cong cua cac so da nhap la: " +tbc);
-   }
+        int tong = 0;
+        for (int i = 1; i <= n; i++) {
+            System.out.print("nhap vao so thu " + i + ": ");
+            int x = sc.nextInt();
+            tong += x;
+        }
+        double tbc = (double) tong / n;
+
+        System.out.println("trung binh cong cua cac so la: " + tbc);
+    }
 }
