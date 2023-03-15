548c54047b7f200918f648d3a05872f19180019d
totu112000 <96477917+totu112000@users.noreply.github.com>
2023-03-15 22:55:18 +0700

    Update Bai9.java

--- BaiThucHanhLab2/src/Bai9.java
+++ BaiThucHanhLab2/src/Bai9.java
@@ -1,3 +1,19 @@
-public class Bai9 {
+import java.util.Scanner;
 
+public class Bai9 {
+    public static void main(String[] args) {
+        String chuoi;
+        char kyTu;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhap vao chuoi bat ki: ");
+        chuoi = scanner.nextLine();
+
+        System.out.println("Cac ki tu co trong chuoi la: ");
+        for (int i = 0; i < chuoi.length(); i++) {
+            kyTu = chuoi.charAt(i);
+
+            System.out.println(kyTu);
+        }
+    }
 }
