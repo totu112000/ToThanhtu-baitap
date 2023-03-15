6ad069ccbb5fd6f2c291128453d3091094665c0f
totu112000 <96477917+totu112000@users.noreply.github.com>
2023-03-15 22:54:54 +0700

    Update Bai10.java

--- BaiThucHanhLab2/src/Bai10.java
+++ BaiThucHanhLab2/src/Bai10.java
@@ -1,3 +1,21 @@
-public class Bai10 {
+import java.util.Scanner;
 
+public class Bai10 {
+    public static void main(String[] aegs) {
+        Scanner scanner = new Scanner(System.in);
+        int LanXuatHien = 0;
+        String chuoi;
+        char KyTu;
+        do {
+            System.out.print("Nhap chuoi: ");
+            chuoi = scanner.nextLine();
+        } while (chuoi.length() > 80);
+        System.out.print("Nhap ki tu can dem: ");
+        KyTu = scanner.next().charAt(0);
+        for (int i = 0; i < chuoi.length(); i++) {
+            if (KyTu == chuoi.charAt(i))
+                LanXuatHien++;
+        }
+        System.out.print("so lan xuat hien ki tu " + KyTu + " trong chuoi " + "la: " + LanXuatHien + " Lan");
+    }
 }
